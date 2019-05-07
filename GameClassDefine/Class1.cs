503024802bd49188179758fd48a55d2bf0a3bd56
hongchan2 <ipeter@naver.com>
2019-05-07 15:10:47 +0900

    5/7 init
    init define

---
+++ GameClassDefine/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClassDefine
+{
+    /*
+        0 : 다이아몬드
+        1 : 루비
+        2 : 사파이어(마린)
+        3 : 에메랄드
+        4 : 오팔
+        */
+
+    public class Card
+    {
+        int cardID;                     // 카드 식별자
+        int[] cardCost = new int[5];    // 구매 시 필요한 보석 개수
+        int cardScore;                  // 카드 점수
+        int cardLevel;                  // 카드 레벨
+        int cardGem;                    // 카드 보석(할인)
+    }
+
+    public class Noble
+    {
+        int nobleID;                    // 귀족카드 식별자
+        int[] nobleCost = new int[5];   // 카드 보석(비용)
+        const int nobleScore = 3;       // 귀족 고정 점수
+    }
+
+    public class Player
+    {
+        int totalScore;                 // 총점수
+        int[] gemSale = new int[5];     // 할인 받을 수 있는 보석 개수
+        List<Card> playerCards = new List<Card>();      // 보유하고 있는 카드
+        List<Noble> playerNoble = new List<Noble>();    // 보유하고 있는 귀족
+        int[] playerGems;               // 보유하고 있는 보석
+
+    }
+}
---
+++ PakcetDefine/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using GameClassDefine;
+
+namespace PacketDefine
+{
+    public enum PacketType
+    {
+        gem = 0,
+        card,
+        turnEnd
+    }
+
+    public enum PacketSendERROR
+    {
+        normal = 0,
+        error
+    }
+
+    [Serializable]
+    public class Packet
+    {
+        public int Length;
+        public int Type;
+
+        public Packet()
+        {
+            this.Length = 0;
+            this.Type = 0;
+        }
+
+        public static byte[] Serialize(Object o)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+
+        public static Object Desserialize(byte[] bt)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            foreach (byte b in bt)
+            {
+                ms.WriteByte(b);
+            }
+
+            ms.Position = 0;
+            BinaryFormatter bf = new BinaryFormatter();
+            Object obj = bf.Deserialize(ms);
+            ms.Close();
+            return obj;
+        }
+    }
+
+    /* 보석 선택 시 */
+    [Serializable]
+    public class Gem : Packet
+    {
+        int[] gems = new int[5];            // 선택된 보석
+        Player[] players = new Player[2];   // 플레이어 1, 2 정보
+    }
+
+    /* 카드 구매 시 */
+    [Serializable]
+    public class SelectCard : Packet
+    {
+        int cardID;                         // 구매한 카드 식별자
+        Player[] players = new Player[2];   // 플레이어 1, 2 정보
+    }
+
+    /* 턴 종료 시 */
+    [Serializable]
+    public class TurnEnd : Packet
+    {
+        int nobleNum;                       // 방문한 귀족 식별자
+        Player[] players = new Player[2];   // 플레이어 1, 2 정보
+    }
+
+}
